2c2704db050ea2548be7bfbfdb0385e263ba7022
dalesio9999 <dalesio99@outlook.com>
2024-08-10 10:49:00 +0200

    Creazione HomeUtente genitore

---
+++ app/src/main/java/com/uniba/pronuntia/HomeUtente.java
@@ -0,0 +1,17 @@
+package com.uniba.pronuntia;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeUtente extends AppCompatActivity {
+
+@Override
+protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_homeutente);
+
+}
+
+
+}
